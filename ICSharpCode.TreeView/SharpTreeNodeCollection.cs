b65766b617b29ca3a26b09493c8920b93ed14d85
wosanek <wosanek@gmail.com>
2021-08-20 11:54:45 +0600

    Добавьте файлы проекта.

---
+++ ICSharpCode.TreeView/SharpTreeNodeCollection.cs
@@ -0,0 +1,225 @@
+﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ICSharpCode.TreeView
+{
+	/// <summary>
+	/// Collection that validates that inserted nodes do not have another parent.
+	/// </summary>
+	public sealed class SharpTreeNodeCollection : IList<SharpTreeNode>, INotifyCollectionChanged
+	{
+        private readonly SharpTreeNode _parent;
+        private List<SharpTreeNode> _list = new List<SharpTreeNode>();
+        private bool _isRaisingEvent;
+		
+		public SharpTreeNodeCollection(SharpTreeNode parent)
+		{
+			_parent = parent;
+		}
+		
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+		{
+			Debug.Assert(!_isRaisingEvent);
+			_isRaisingEvent = true;
+			try {
+				_parent.OnChildrenChanged(e);
+				if (CollectionChanged != null)
+					CollectionChanged(this, e);
+			} finally {
+				_isRaisingEvent = false;
+			}
+		}
+
+        private void ThrowOnReentrancy()
+		{
+			if (_isRaisingEvent)
+				throw new InvalidOperationException();
+		}
+
+        private void ThrowIfValueIsNullOrHasParent(SharpTreeNode node)
+		{
+			if (node == null)
+				throw new ArgumentNullException("node");
+			if (node.ModelParent != null)
+				throw new ArgumentException("The node already has a parent", "node");
+		}
+		
+		public SharpTreeNode this[int index] {
+			get {
+				return _list[index];
+			}
+			set {
+				ThrowOnReentrancy();
+				var oldItem = _list[index];
+				if (oldItem == value)
+					return;
+				ThrowIfValueIsNullOrHasParent(value);
+				_list[index] = value;
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, oldItem, index));
+			}
+		}
+		
+		public int Count {
+			get { return _list.Count; }
+		}
+		
+		bool ICollection<SharpTreeNode>.IsReadOnly {
+			get { return false; }
+		}
+		
+		public int IndexOf(SharpTreeNode node)
+		{
+			if (node == null || node.ModelParent != _parent)
+				return -1;
+			else
+				return _list.IndexOf(node);
+		}
+		
+		public void Insert(int index, SharpTreeNode node)
+		{
+			ThrowOnReentrancy();
+			ThrowIfValueIsNullOrHasParent(node);
+			_list.Insert(index, node);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, node, index));
+		}
+		
+		public void InsertRange(int index, IEnumerable<SharpTreeNode> nodes)
+		{
+			if (nodes == null)
+				throw new ArgumentNullException("nodes");
+			ThrowOnReentrancy();
+			List<SharpTreeNode> newNodes = nodes.ToList();
+			if (newNodes.Count == 0)
+				return;
+			foreach (SharpTreeNode node in newNodes) {
+				ThrowIfValueIsNullOrHasParent(node);
+			}
+			_list.InsertRange(index, newNodes);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newNodes, index));
+		}
+		
+		public void RemoveAt(int index)
+		{
+			ThrowOnReentrancy();
+			var oldItem = _list[index];
+			_list.RemoveAt(index);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItem, index));
+		}
+		
+		public void RemoveRange(int index, int count)
+		{
+			ThrowOnReentrancy();
+			if (count == 0)
+				return;
+			var oldItems = _list.GetRange(index, count);
+			_list.RemoveRange(index, count);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems, index));
+		}
+		
+		public void Add(SharpTreeNode node)
+		{
+			ThrowOnReentrancy();
+			ThrowIfValueIsNullOrHasParent(node);
+			_list.Add(node);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, node, _list.Count - 1));
+		}
+		
+		public void AddRange(IEnumerable<SharpTreeNode> nodes)
+		{
+			InsertRange(this.Count, nodes);
+		}
+		
+		public void Clear()
+		{
+			ThrowOnReentrancy();
+			var oldList = _list;
+			_list = new List<SharpTreeNode>();
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldList, 0));
+		}
+		
+		public bool Contains(SharpTreeNode node)
+		{
+			return IndexOf(node) >= 0;
+		}
+		
+		public void CopyTo(SharpTreeNode[] array, int arrayIndex)
+		{
+			_list.CopyTo(array, arrayIndex);
+		}
+		
+		public bool Remove(SharpTreeNode item)
+		{
+			int pos = IndexOf(item);
+			if (pos >= 0) {
+				RemoveAt(pos);
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public IEnumerator<SharpTreeNode> GetEnumerator()
+		{
+			return _list.GetEnumerator();
+		}
+		
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return _list.GetEnumerator();
+		}
+		
+		public void RemoveAll(Predicate<SharpTreeNode> match)
+		{
+			if (match == null)
+				throw new ArgumentNullException("match");
+			ThrowOnReentrancy();
+			int firstToRemove = 0;
+			for (int i = 0; i < _list.Count; i++) {
+				bool removeNode;
+				_isRaisingEvent = true;
+				try {
+					removeNode = match(_list[i]);
+				} finally {
+					_isRaisingEvent = false;
+				}
+				if (!removeNode) {
+					if (firstToRemove < i) {
+						RemoveRange(firstToRemove, i - firstToRemove);
+						i = firstToRemove - 1;
+					} else {
+						firstToRemove = i + 1;
+					}
+					Debug.Assert(firstToRemove == i + 1);
+				}
+			}
+			if (firstToRemove < _list.Count) {
+				RemoveRange(firstToRemove, _list.Count - firstToRemove);
+			}
+		}
+    }
+}
---
+++ ICSharpCode.TreeView/SharpTreeViewItem.cs
@@ -0,0 +1,204 @@
+﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace ICSharpCode.TreeView
+{
+	public class SharpTreeViewItem : ListViewItem
+	{
+		static SharpTreeViewItem()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(SharpTreeViewItem),
+			                                         new FrameworkPropertyMetadata(typeof(SharpTreeViewItem)));
+		}
+
+		public SharpTreeNode Node
+		{
+			get { return DataContext as SharpTreeNode; }
+		}
+
+		public SharpTreeNodeView NodeView { get; internal set; }
+		public SharpTreeView ParentTreeView { get; internal set; }
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			switch (e.Key) {
+				case Key.F2:
+					if (Node.IsEditable && ParentTreeView != null && ParentTreeView.SelectedItems.Count == 1 && ParentTreeView.SelectedItems[0] == Node) {
+						Node.IsEditing = true;
+						e.Handled = true;
+					}
+					break;
+				case Key.Escape:
+					if (Node.IsEditing) {
+						Node.IsEditing = false;
+						e.Handled = true;
+					}
+					break;
+			}
+		}
+
+		#region Mouse
+
+        private Point _startPoint;
+        private bool _wasSelected;
+        private bool _wasDoubleClick;
+		
+		//protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+		//{
+		//	if (!ParentTreeView.CanDragAndDrop)
+		//	{
+		//		OnDoubleClick(e);
+		//		e.Handled = true;
+		//		return;
+		//	}
+
+		//	base.OnMouseDoubleClick(e);
+		//}
+
+		public void ExecuteOnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+			this.OnMouseLeftButtonDown(e);
+
+		}
+
+		protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+		{
+			_wasSelected = IsSelected;
+			if (!IsSelected) {
+				base.OnMouseLeftButtonDown(e);
+			}
+			//if (!ParentTreeView.CanDragAndDrop)
+			//	_wasDoubleClick = false;
+			//else 
+			if (Mouse.LeftButton == MouseButtonState.Pressed) {
+				_startPoint = e.GetPosition(null);
+				CaptureMouse();
+
+				if (e.ClickCount == 2) {
+					_wasDoubleClick = true;
+				}
+			}
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			if (IsMouseCaptured) {
+				var currentPoint = e.GetPosition(null);
+				if (Math.Abs(currentPoint.X - _startPoint.X) >= SystemParameters.MinimumHorizontalDragDistance ||
+				    Math.Abs(currentPoint.Y - _startPoint.Y) >= SystemParameters.MinimumVerticalDragDistance) {
+
+					var selection = ParentTreeView.GetTopLevelSelection().ToArray();
+					Node.StartDrag(this, selection);
+				}
+			}
+		}
+
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			if (Node == null)
+			{
+				// Ignore it: Node is sometimes null
+			}
+			else if (_wasDoubleClick) 
+			{
+				_wasDoubleClick = false;
+				OnDoubleClick(e);
+				//Node.ActivateItem(e);
+
+				//if (!e.Handled) 
+				//{
+				//	if (!Node.IsRoot || ParentTreeView.ShowRootExpander) 
+				//	{
+				//		Node.IsExpanded = !Node.IsExpanded;
+				//	}
+				//}
+			}
+			
+			ReleaseMouseCapture();
+			if (_wasSelected) {
+				_wasSelected = false;
+				//base.OnMouseLeftButtonDown(e);
+				// Make sure the TV doesn't steal focus when double clicking something that will
+				// trigger setting focus to eg. the text editor.
+				if (!_ignoreOnMouseLeftButtonDown)
+					base.OnMouseLeftButtonDown(e);
+			}
+		}
+		bool _ignoreOnMouseLeftButtonDown = false;
+		void OnDoubleClick(RoutedEventArgs e)
+		{
+			_ignoreOnMouseLeftButtonDown = true;
+			if (Node == null)
+				return;
+			Node.ActivateItem(e);
+			if (!e.Handled)
+			{
+				if (!Node.IsRoot || ParentTreeView.ShowRootExpander)
+				{
+					Node.IsExpanded = !Node.IsExpanded;
+				}
+			}
+		}
+
+		protected override void OnContextMenuOpening(ContextMenuEventArgs e)
+		{
+			if (Node != null)
+				Node.ShowContextMenu(e);
+		}
+
+        #endregion
+
+        #region Drag and Drop
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            ParentTreeView.HandleDragEnter(this, e);
+			e.Handled = true;
+		}
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            ParentTreeView.HandleDragOver(this, e);
+			e.Handled = true;
+		}
+
+		protected override void OnDrop(DragEventArgs e)
+        {
+            ParentTreeView.HandleDrop(this, e);
+			e.Handled = true;
+        }
+
+        protected override void OnDragLeave(DragEventArgs e)
+        {
+            ParentTreeView.HandleDragLeave(this, e);
+			e.Handled = true;
+		}
+
+		#endregion
+	}
+}
